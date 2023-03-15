d3f9bd18de838561a2ca13e1c5dd1d20a3f85d5c
LudovicaCi <lcocchella@yahoo.it>
2023-03-15 15:31:38 +0100

    Creation of the scheleton of the command line interface

--- src/main/java/it/unipi/dii/aide/mircv/cli/Main.java
+++ src/main/java/it/unipi/dii/aide/mircv/cli/Main.java
@@ -1,23 +1,7 @@
 package it.unipi.dii.aide.mircv.cli;
 
-import it.unipi.dii.aide.mircv.index.FileManagement;
-import it.unipi.dii.aide.mircv.index.Merger;
-import it.unipi.dii.aide.mircv.index.SPIMI;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-import static it.unipi.dii.aide.mircv.index.Util.formatTime;
-
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-
-        FileManagement fm = new FileManagement();
-        SPIMI s = new SPIMI();
-
-        fm.createFileDocumentIndex();
-        s.invert();
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
